bfdb5cee51d433d54ca3b039614ccce1e0a60c95
kailermai <kailermaioff@gmail.com>
2024-01-30 02:08:49 +0800

    added function createGrid to execute the button press

--- etch.js
+++ etch.js
@@ -1,6 +1,31 @@
 let answer;
+let sizesq;
 
 const container = document.querySelector(".container");
+
+//Function which removes previously created grid and creates a new grid
+function createGrid(num) {
+  //Removes all previous squares
+  while (container.firstChild) {
+    container.removeChild(container.lastChild);
+  }
+  //Adds in new squares
+  const totalnum = num * num;
+  for (let i = 0; i < totalnum + 1; i++) {
+    const sqdiv = document.createElement("div");
+    sqdiv.className = "newSquare";
+    sqdiv.style.height = sizesq.toString() + "px";
+    sqdiv.style.width = sizesq.toString() + "px";
+    container.appendChild(sqdiv);
+  }
+
+  const newSquares = document.querySelectorAll(".newSquare");
+  newSquares.forEach((newSquare) => {
+    newSquare.addEventListener("mousemove", () => {
+      newSquare.style.backgroundColor = "black";
+    });
+  });
+}
 
 //Creating n numbers of boxes
 for (let i = 0; i < 257; i++) {
@@ -27,4 +52,6 @@ btn.addEventListener("click", () => {
       "Number of squares per side for the new grid:\nEnter a number from 1 to 100 only."
     );
   } while (isNaN(answer) || Number(answer) <= 0 || Number(answer) > 100);
+  sizesq = Math.round((800.0 / answer) * 1000) / 1000;
+  createGrid(answer);
 });
