474553857768778086099dea8e6675d23a1c38f0
kailermai <kailermaioff@gmail.com>
2024-01-30 01:03:14 +0800

    added borders to the squares, created a button for new grid

--- etch.js
+++ etch.js
@@ -1,7 +1,30 @@
+let answer;
+
 const container = document.querySelector(".container");
 
+//Creating n numbers of boxes
 for (let i = 0; i < 257; i++) {
   const sqdiv = document.createElement("div");
   sqdiv.className = "square";
   container.appendChild(sqdiv);
 }
+
+//Changes the colour of the square when hovered over
+const squares = document.querySelectorAll(".square");
+squares.forEach((square) => {
+  square.addEventListener("mousemove", () => {
+    square.style.backgroundColor = "black";
+  });
+});
+
+//Button that allows the user to input and change the grid
+const btn = document.querySelector("button");
+btn.textContent = "Click me to adjust the grid!";
+btn.addEventListener("click", () => {
+  //Check for non number inputs by the user
+  do {
+    answer = prompt(
+      "Number of squares per side for the new grid:\nEnter a number from 1 to 100 only."
+    );
+  } while (isNaN(answer) || Number(answer) <= 0 || Number(answer) > 100);
+});
